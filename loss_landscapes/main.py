41c01cb12ed2d8e9e48eafa52320b4cb5b1fbe62
Alfonsas <alpha358@gmail.com>
2020-04-06 16:26:35 +0300

    device

--- loss_landscapes/main.py
+++ loss_landscapes/main.py
@@ -229,7 +229,7 @@ def planar_interpolation(model_start: typing.Union[torch.nn.Module, ModelWrapper
 
 
 # noinspection DuplicatedCode
-def random_plane(model: typing.Union[torch.nn.Module, ModelWrapper], metric: Metric, distance=1, steps=20,
+def random_plane(model: typing.Union[torch.nn.Module, ModelWrapper], metric: Metric, DEVICE, distance=1, steps=20,
                  normalization='filter', deepcopy_model=False) -> np.ndarray:
     """
     Returns the computed value of the evaluation function applied to the model or agent along a planar
@@ -271,9 +271,9 @@ def random_plane(model: typing.Union[torch.nn.Module, ModelWrapper], metric: Met
     """
     model_start_wrapper = wrap_model(copy.deepcopy(model) if deepcopy_model else model)
 
-    start_point = model_start_wrapper.get_module_parameters()
-    dir_one = rand_u_like(start_point)
-    dir_two = orthogonal_to(dir_one)
+    start_point = model_start_wrapper.get_module_parameters().to(DEVICE)
+    dir_one = rand_u_like(start_point).to(DEVICE)
+    dir_two = orthogonal_to(dir_one).to(DEVICE)
 
     if normalization == 'model':
         dir_one.model_normalize_(start_point)
