d173af53f3bd04b2f04eaca9d28a7eb4dc7983ce
Alfonsas <alpha358@gmail.com>
2020-04-06 16:29:32 +0300

    device

--- loss_landscapes/main.py
+++ loss_landscapes/main.py
@@ -272,8 +272,8 @@ def random_plane(model: typing.Union[torch.nn.Module, ModelWrapper], metric: Met
     model_start_wrapper = wrap_model(copy.deepcopy(model) if deepcopy_model else model)
 
     start_point = model_start_wrapper.get_module_parameters().to(DEVICE)
-    dir_one = rand_u_like(start_point).to(DEVICE)
-    dir_two = orthogonal_to(dir_one).to(DEVICE)
+    dir_one = rand_u_like(start_point)
+    dir_two = orthogonal_to(dir_one)
 
     if normalization == 'model':
         dir_one.model_normalize_(start_point)
